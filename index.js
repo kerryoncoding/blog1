802cb909809c3e6083ad23614d634766defaba00
Kerry Guarino <kerry.guarino@gmail.com>
2023-06-07 07:31:34 -0400

    wrapping up example

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const message = document.querySelector("#greeting")
 form.addEventListener("submit", (e)=> {
    e.preventDefault();
    let input = e.target[0].value
-
-   //const user = document.querySelector("#name")
    message.innerHTML = `Welcome ${input}`
 })
 
