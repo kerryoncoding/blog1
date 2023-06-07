b007f34a16fb24f0947e6eceaa778727ee6aceec
Kerry Guarino <kerry.guarino@gmail.com>
2023-06-07 06:34:49 -0400

    forgot closing p tag

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 
-const btnZero = document.querySelector("#changeZero")
-const btnOne = document.querySelector("#changeOne")
-const btnTwo = document.querySelector("#changeTwo")
+const btnText = document.querySelector("#changeText")
+const btnContent = document.querySelector("#changeContent")
+const btnHtml= document.querySelector("#changeHtml")
 
-btnZero.addEventListener("click", ()=> {
-   console.log("layer Zero:")
+btnText.addEventListener("click", ()=> {
    let changeZero = document.querySelector("#layer-zero") 
    console.log(changeZero.innerText)
    console.log(changeZero.textContent)
    console.log(changeZero.innerHTML)
 })
 
-btnOne.addEventListener("click", ()=> {
-   console.log("layer One:")
+btnContent.addEventListener("click", ()=> {
    let changeOne = document.querySelector("#layer-one") 
    console.log(changeOne.innerText)
    console.log(changeOne.textContent)
    console.log(changeOne.innerHTML)
 })
 
-btnTwo.addEventListener("click", ()=> {
-   console.log("layer Two:")
+btnHtml.addEventListener("click", ()=> {
    let changeTwo = document.querySelector("#layer-two") 
-   console.log(changeTwo.innerText)
-   console.log(changeTwo.textContent)
-   console.log(changeTwo.innerHTML)
+   changeTwo.innerText = "innerText change to layer tww"
+   changeTwo.textContent = "textContent change to layer two"
+   changeTwo.innerHTML = "innerHTML change to layer two"
 })
 
