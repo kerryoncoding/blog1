8eac45ed788aa8eea317c6cdbb13c7440ef5d56b
Kerry Guarino <kerry.guarino@gmail.com>
2023-06-07 07:15:32 -0400

    changed to show form

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 
-const btnText = document.querySelector("#changeText")
-const btnContent = document.querySelector("#changeContent")
-const btnHtml= document.querySelector("#changeHtml")
 
-btnText.addEventListener("click", ()=> {
-   let changeZero = document.querySelector("#layer-zero") 
-   console.log(changeZero.innerText)
-   console.log(changeZero.textContent)
-   console.log(changeZero.innerHTML)
-})
+const form = document.querySelector("#myForm")
+const message = document.querySelector("#greeting")
 
-btnContent.addEventListener("click", ()=> {
-   let changeOne = document.querySelector("#layer-one") 
-   console.log(changeOne.innerText)
-   console.log(changeOne.textContent)
-   console.log(changeOne.innerHTML)
-})
+form.addEventListener("submit", (e)=> {
+   e.preventDefault();
+   let input = e.target[0].value
 
-btnHtml.addEventListener("click", ()=> {
-   let changeTwo = document.querySelector("#layer-two") 
-   changeTwo.innerText = "innerText change to layer tww"
-   changeTwo.textContent = "textContent change to layer two"
-   changeTwo.innerHTML = "innerHTML change to layer two"
+   //const user = document.querySelector("#name")
+   message.innerHTML = `Welcome ${input}`
 })
 
